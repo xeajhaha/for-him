ff0f602ed783287dd5f1df68e5a903f83d25d23d
xea <yna.rempillo0209@gmail.com>
2025-02-13 20:45:44 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function sayYes() {
-    document.getElementById('proposalText').innerText = "hehehe mwa ❤️";
-    document.getElementById('subText').innerText = "dapat lang hahaha";
+    document.getElementById('proposalText').innerText = "Hehe mwa ❤️";
+    document.getElementById('subText').innerText = "Dapat lang!";
     document.getElementById('valentineGif').src = "https://media.giphy.com/media/Xv0aLD5YZyLvI2l4ts/giphy.gif";
-    
+
     // Hide the buttons
     document.getElementById('buttons').style.display = "none";
 }
@@ -13,4 +13,11 @@ function moveNo(button) {
     button.style.position = "absolute";
     button.style.left = `${x}px`;
     button.style.top = `${y}px`;
+}
+
+// Check if the device is touch-based (for mobile)
+if ("ontouchstart" in document.documentElement) {
+    document.querySelector(".no").addEventListener("touchstart", function() {
+        moveNo(this);
+    });
 }
